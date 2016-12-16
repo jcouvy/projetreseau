e18d5c7cbbebd222d0c2d1d67cf6fa8c7bac69fa
jcouvy <julien.couvy@gmail.com>
2016-12-16 12:13:01 +0100

    newgame functions + turn

--- serveur.py
+++ serveur.py
@@ -1,19 +1,45 @@
 import socket, select
 import re
 import sys
+import random
 
 from grid import *
 
+class Game:
 
-def init_grids():
-    return Grid(), Grid(), Grid()
+    def __init__(self):
+        playerOne = None
+        playerTwo = None
+        gridOne = Grid()
+        gridTwo = Grid()
+        gridObs = Grid()
+        turn = -1
+
+    def start(self, playerA, playerB):
+        self.playerOne = playerA
+        self.playerTwo = playerB
+        self.turn = random.randint(1,2)
+
+        self.playerOne.send(b'GRID 000000000\n')
+        self.playerTwo.send(b'GRID 000000000\n')
+
+    def send_turn(self):
+        assert(self.turn == 1 or self.turn == 2)
+        if self.turn == 1:
+            self.playerOne.send(b'PLAY\n')
+            self.playerTwo.send(b'WAIT\n')
+        else:
+            self.playerOne.send(b'WAIT\n')
+            self.playerTwo.send(b'PLAY\n')
+
 
 def start_server():
-
     HOST = ''
     PORT = 8888
     RECV_BUFFER = 4096
+
     USER_DICT = {}
+    PLAYER_DICT = {}
 
     readers_list = []
 
@@ -38,6 +64,18 @@ def start_server():
                 USER_DICT.update({str(ip):socket.gethostname()})
                 print ('Client {0} ({1}) connected'.format(USER_DICT.get(ip), ip))
 
+                if (len(readers_list) - 1) % 2 == 0:
+                    newGame = Game()
+                    newGame.start(readers_list[1], readers_list[2])
+                    newGame.send_turn()
+                    clientA = readers_list[1].getsockname()
+                    clientB = readers_list[2].getsockname()
+                    clientA_IP = re.sub(r':*[a-z]*:', '', clientA[0])
+                    clientB_IP = re.sub(r':*[a-z]*:', '', clientB[0])
+                    print ('New game started \n '\
+                           'Player 1 - {} ({}) \n '\
+                           'Player 2 - {} ({})').format(USER_DICT.get(clientA_IP), clientA_IP,
+                                                        USER_DICT.get(clientB_IP), clientB_IP)
 
             else:
                 data = sock.recv(RECV_BUFFER)
@@ -47,7 +85,7 @@ def start_server():
                     print ('Client {0} ({1}) disconnect'.format(USER_DICT.get(ip), ip))
                     readers_list.remove(sock)
                     sock.close()
-                    continue
+
 
 if __name__ == '__main__':
     sys.exit(start_server())
