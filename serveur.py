d37f861cd58b5f99057fe7ae53cb7c35a03f45ee
jcouvy <julien.couvy@gmail.com>
2016-12-16 14:31:54 +0100

    Adding User class

--- serveur.py
+++ serveur.py
@@ -5,8 +5,18 @@ import random
 
 from grid import *
 
-class Game:
+class User:
+    def __init__(self, socket, name, ip):
+        socket.setblocking(0) # Keeps control in case of error/timeout
+        self.socket = socket
+        self.name   = name
+        self.ip     = ip
 
+    # Necessary with select based TCP server
+    def fileno(self):
+        return self.socket.fileno()
+
+class Game:
     def __init__(self):
         playerOne = None
         playerTwo = None
@@ -20,71 +30,65 @@ class Game:
         self.playerTwo = playerB
         self.turn = random.randint(1,2)
 
-        self.playerOne.send(b'GRID 000000000\n')
-        self.playerTwo.send(b'GRID 000000000\n')
+        self.playerOne.socket.send(b'GRID 000000000\n')
+        self.playerTwo.socket.send(b'GRID 000000000\n')
 
     def send_turn(self):
         assert(self.turn == 1 or self.turn == 2)
         if self.turn == 1:
-            self.playerOne.send(b'PLAY\n')
-            self.playerTwo.send(b'WAIT\n')
+            self.playerOne.socket.send(b'PLAY\n')
+            self.playerTwo.socket.send(b'WAIT\n')
         else:
-            self.playerOne.send(b'WAIT\n')
-            self.playerTwo.send(b'PLAY\n')
+            self.playerOne.socket.send(b'WAIT\n')
+            self.playerTwo.socket.send(b'PLAY\n')
 
 
 def start_server():
+
     HOST = ''
     PORT = 8888
     RECV_BUFFER = 4096
-
-    USER_DICT = {}
-    PLAYER_DICT = {}
-
-    readers_list = []
 
     server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen(1)
 
-    readers_list.append(server_socket)
+    connection_list = []
+    connection_list.append(server_socket)
 
     print('Server started on port {0}').format(PORT)
 
     while True:
-        read_socket, _ , _ = select.select(readers_list, [], [])
+        read_sockets, _ , _ = select.select(connection_list, [], [])
 
-        for sock in read_socket:
+        for client in read_sockets:
+            if client is server_socket:
+                new_socket, addr = server_socket.accept()
+                user = User(new_socket,
+                            socket.gethostname(),
+                            socket.gethostbyname(socket.gethostname()))
+                connection_list.append(user)
+                print ('Client {} ({}) connected').format(user.name,
+                                                          user.ip)
 
-            if sock == server_socket:
-                conn, addr = server_socket.accept()
-                readers_list.append(conn)
-                ip = re.sub(r':*[a-z]*:', '', addr[0])
-                USER_DICT.update({str(ip):socket.gethostname()})
-                print ('Client {0} ({1}) connected'.format(USER_DICT.get(ip), ip))
-
-                if (len(readers_list) - 1) % 2 == 0:
-                    newGame = Game()
-                    newGame.start(readers_list[1], readers_list[2])
-                    newGame.send_turn()
-                    clientA = readers_list[1].getsockname()
-                    clientB = readers_list[2].getsockname()
-                    clientA_IP = re.sub(r':*[a-z]*:', '', clientA[0])
-                    clientB_IP = re.sub(r':*[a-z]*:', '', clientB[0])
-                    print ('New game started \n '\
-                           'Player 1 - {} ({}) \n '\
-                           'Player 2 - {} ({})').format(USER_DICT.get(clientA_IP), clientA_IP,
-                                                        USER_DICT.get(clientB_IP), clientB_IP)
-
+                if (len(connection_list) - 1) % 2 == 0:
+                    game = Game()
+                    game.start(connection_list[1], connection_list[2])
+                    game.send_turn()
+                    print ('New game started \n' \
+                           ' Player 1: {}\n'     \
+                           ' Player 2: {}').format(game.playerOne.name,
+                                                   game.playerTwo.name)
             else:
-                data = sock.recv(RECV_BUFFER)
+                data = client.socket.recv(RECV_BUFFER)
                 if data:
                         print ('Received data from client')
                 else:
-                    print ('Client {0} ({1}) disconnect'.format(USER_DICT.get(ip), ip))
-                    readers_list.remove(sock)
-                    sock.close()
+                    print ('Client {} ({}) disconnected').format(client.name,
+                                                                 client.ip)
+                    connection_list.remove(client)
+                    client.socket.close()
 
 
 if __name__ == '__main__':
